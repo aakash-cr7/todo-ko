1cbbbe50c52196610b4f6d4d45c091be29bafd71
aakash-cr7 <aakash.tyg93@gmail.com>
2016-07-04 01:39:38 +0530

    Add functionality to add new todo items.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
 (function() {
     
     // Single ToDo item
-    var Todo = function(title) {
-        
+    var Todo = function(title, completed = false) {
+        this.title = ko.observable(title);
+        this.completed = ko.observable(completed);
+        this.editMode = ko.observable(false);
     }
 
     var ViewModel = function() {
+        this.current = ko.observable("");
+
+        this.todoList = ko.observableArray([]);
+        // Add a new todo item 
+        this.add = function() {
+            var current = this.current().trim();
+
+            if (current) {
+                // push new todo item to the todoList
+                this.todoList.push( new Todo(current) );
+                // make current '' after pushing it to the list
+                this.current('');
+            }
+        }
     }
-})();
+
+    // Bind a new instance of the ViewModel to our page
+    var viewModel = new ViewModel();
+    ko.applyBindings(viewModel);
+}() );
